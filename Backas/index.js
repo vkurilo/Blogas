b2e262ef113e524801784b70d3f0520fb111d807
vkurilo <92544044+vkurilo@users.noreply.github.com>
2022-03-02 21:55:10 +0200

    work work work

--- Backas/index.js
+++ Backas/index.js
@@ -5,15 +5,98 @@ import * as fs from "fs";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const filePath = "./db/data.json";
+const filePath = "./dp/data.json";
 
 
-app.post('/post-request', (req,res) => { 
-    console.log(req.body)
-    res.send(req.body)
+
+
+
+
+
+
+
+//gaunam visa json
+app.get('/', (req,res) => { 
+    fs.readFile(filePath,'utf8', (err,data) => {
+        let info = JSON.parse(data)
+        if(err){
+            req.json({status:'failed', message:"Nepavyko perskaityti failo"})
+        }else{ 
+            res.json({status:'success', jsonResp: info })
+            
+        }
+    })
 })
 
 
+//gaunam id 
+app.get('/:id', (req,res) => {
+    let id = req.params.id
+  
+    fs.readFile(filePath, 'utf8', (err,data) => {
+      if(err){
+        res.json({status:'failed', message: 'Nepavyko perskaityti failo'})
+        return false 
+      }
+      let json = JSON.parse(data)
+      let found = false 
+  
+      json.forEach((el,index) => { 
+        if(el.id == id){
+          res.json({status:'success', message:'Sekmingai perskaityti duomenys', jsonResp:el})
+          found = true 
+        }
+      })
+      if(!found){
+        res.json({status:'failed', message:'Nepavyko surasti tokio id' })
+      }
+    })
+  
+  
+  
+  })
+//issaugom
+app.post("/save-request", (req, res) => {
+    let masyvas = [];
+  
+    fs.access(filePath, (err) => {
+      if (err) {
+        req.body.id = 0;
+        masyvas.push(req.body);
+        fs.writeFile(filePath, JSON.stringify(masyvas), "utf8", (err) => {
+          if (!err) {
+            res.json({status:'success', message: "Informacija issaugota" });
+          } else {
+            res.json({status: 'failed', message: "Nepavyko sukurti aplanko" });
+          }
+        });
+      } else {
+        fs.readFile(filePath, "utf8", (err, val) => {
+          if (err) {
+            res.json({status:'failed', message: "ivyko klaida" });
+            return false;
+          }
+          let json = JSON.parse(val);
+          if(json.length == 0){
+            req.body.id = 0
+          }
+          else{
+            req.body.id = json[json.length - 1].id + 1
+          }
+          
+          json.push(req.body);
+          let info = JSON.stringify(json)
+          fs.writeFile(filePath, info, "utf8", (err) => {
+            if (!err) {
+              res.json({status:'success', message: "Informacija issaugota", jsonResp: info });
+            } else {
+              res.json({ message: "Nepavyko sukurti aplanko" });
+            }
+          });
+        });
+      }
+    });
+})
 
 
 
--- Frontas/index.js
+++ Frontas/index.js
@@ -1,5 +1,16 @@
+
+
+let hide = document.querySelector('#invisible').classList
+let galery = document.querySelector('#galery').classList
+
 document.querySelector('.newBlog').addEventListener('click', () => {
-    document.querySelector('#invisible').classList.remove('d-none')
+   if(hide.value == 'd-none')
+   { document.querySelector('#invisible').classList.remove('d-none')
+   document.querySelector('#galery').classList.add('d-none')}
+   else{
+    document.querySelector('#invisible').classList.add('d-none')
+    document.querySelector('#galery').classList.remove('d-none')
+   }
 })
 
 
@@ -11,12 +22,12 @@ let name = document.querySelector('#invisible input[name="pavadinimas"]').value
 document.querySelector('#invisible input[name="pavadinimas"]').value = ""
 let about = document.querySelector('#invisible textarea[name="aprasymas"]').value
 document.querySelector('#invisible textarea[name="aprasymas"]').value = ""
-
-
-fetch('http://localhost:3001/post-request',{
+let date = document.querySelector('#invisible input[name="date"]').value
+document.querySelector('#invisible input[name="date"]').value = ''
+fetch('http://localhost:3001/save-request',{
     method:'POST',
     headers: {"Content-type" : 'application/json'},
-    body:JSON.stringify({name,about})
+    body:JSON.stringify({name,about,date})
 })
 .then(resp => resp.json())
 .then(jsonObject => {
@@ -25,4 +36,14 @@ fetch('http://localhost:3001/post-request',{
 
 
 
+
 })
+
+
+
+fetch('http://localhost:3001')
+.then(resp => resp.json())
+.then((jsonObject) => {
+console.log(jsonObject)
+})
+
